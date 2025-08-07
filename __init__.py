27532400667d19c639e16667bd892ad1d3411008
MahdiehRema <78417870+MahdiehRema@users.noreply.github.com>
2025-08-07 15:26:32 +0200

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
-# Package info
-__title__ = "PVD-Dashboard-Ap"
-__version__ = "0.0.0"
-__author__ = "Mahdieh Rezaeimahboub"
-
+from .db_engine import *
---
+++ db_engine.py
@@ -0,0 +1,29 @@
+
+import os
+import sqlalchemy
+import streamlit as st
+import bcavault
+
+
+def engine_oracle(db_user: str = "cidp_out") -> sqlalchemy.engine.base.Engine:
+    """Baue eine Engine mittels SQLAlchemy für Oracle-Datenbanken.
+
+    :param secret: Beinhaltet die secrets für die zu bauende Engine
+    :returns: Eine SQLAlchemy-Engine
+    """
+    kv_engine = bcavault.KvSecretsEngine("analytics")
+    secret = kv_engine.read(f"database/vtdwh/{db_user}").to_dict()
+    host = secret.get("host")
+    port = secret.get("port")
+    service = secret.get("service")
+    user = secret.get("user")
+    if db_user == "streamlit":
+        password = os.getenv("ORACLE_STREAMLIT_PW")
+    else:
+        password = secret.get("password")
+
+    connection_string = (
+        f"oracle+oracledb://{user}:{password}@{host}:{port}/?service_name={service}"
+    )
+    engine = sqlalchemy.create_engine(connection_string)
+    return engine
